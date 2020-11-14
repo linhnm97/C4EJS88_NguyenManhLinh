ca6e279a80e59a879957833d875fc7d64649a562
linhnm97 <manhlinhnguyen51@gmail.com>
2020-11-14 23:56:51 +0700

    Update

--- Session 3/Honework/hwork-11/hwork11.js
+++ Session 3/Honework/hwork-11/hwork11.js
@@ -1,8 +1,9 @@
 let number = prompt (`Enter a sequence of number: `);
 let numberArr = number.split(",");
+let numberArr2 = [];
 for (i= 0 ; i <= numberArr.length -1 ; i++) {
     if (numberArr[i] % 2 !== 0) {
-        alert (`${numberArr} => ${numberArr[i]}`);
+        numberArr2.push(numberArr[i]);
     }
-    
 }
+alert (`${numberArr} => ${numberArr2}`);
